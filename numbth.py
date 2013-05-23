3145881b817256f92c8388aee4a5036432ec531e
Tobit Raff <0xtobit@gmail.com>
2013-05-22 21:31:18 -0400

    numbth is pep8 compliant and removed shebangs

--- numbth.py
+++ numbth.py
@@ -1,23 +1,18 @@
-#!/usr/bin/env python
-
 from functools import reduce
 
-def gcd(a,b):
-    """
-    Returns greatest common divisor using Euclid's Algorithm.
-    """
+
+def gcd(a, b):
+    """Returns greatest common divisor using Euclid's Algorithm."""
     while b:
-        a,b = b, a % b
+        a, b = b, a % b
     return a
 
-def lcm_2(a,b):
-    """
-    Returns lowest common multiple.
-    """
-    return a*b // gcd(a,b)
+
+def lcm_2(a, b):
+    """Returns lowest common multiple."""
+    return a*b // gcd(a, b)
+
 
 def lcm(*args):
-    """
-    Returns lowest commone multipel of args.
-    """
+    """Returns lowest commone multipel of args."""
     return reduce(lcm_2, args)
